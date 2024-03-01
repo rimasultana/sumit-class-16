76b1398d621ad63c671acdb5dccf9302d18b0517
rimasultana <rimasultana20225@gmail.com>
2024-03-01 19:10:14 +0600

    concat method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const text = "LABU, KOMOLA,APPLE";
-let part =text.slice(6,12);
-document.getElementById("demo").innerHTML = part;
+let x1 = "Hello";
+let x2 = 'World';
+let x3 =x1.concat(" ",x2);
+document.getElementById("demo").innerHTML = x3;
