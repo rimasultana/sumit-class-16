bebe45e0abc8cb85790eaccf3bc126159a20ca6b
rimasultana <rimasultana20225@gmail.com>
2024-03-01 19:22:39 +0600

    padStart

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let x1 = "       Hello World         " ;
-
-let x2 =x1.trim();
-
-document.getElementById("demo").innerHTML = "lenth x1 =" + x1.length + "<br> lenth x2 = " + x2.length;
+let x = "5";
+x =x.padStart(5,"0");
+document.getElementById("demo").innerHTML = x;
